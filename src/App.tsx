4b5ef7c2a096561b1ef1c296c26c15cef180550f
David Rose <doppler@gmail.com>
2021-07-19 23:43:37 -0400

    added random-circles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import './App.css';
-import App from './wallpapers/graph';
+import App from './wallpapers/random-circles';
 
 console.log(ReactDOMServer.renderToString(<App />));
 export default App;
