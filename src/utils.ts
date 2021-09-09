cf467db97b4e08cf8a25b476cfbd2cd9f64a58ab
David Rose <doppler@gmail.com>
2021-09-08 20:33:37 -0400

    refactor

--- src/utils.ts
+++ src/utils.ts
@@ -24,7 +24,18 @@ export const interpolate = (divs: number) => {
   return [0, ...[...Array(divs).keys()].map(k => (1 / divs) * (k + 1))];
 };
 
-export const cubicBezierPoints = (
+/**
+ * Return a list of x,y coordinates for a quadratic bezier curve given start, control and end points
+ * @param x0 start x
+ * @param y0 start y
+ * @param x1 control point x
+ * @param y1 control point y
+ * @param x2 end x
+ * @param y2 end y
+ * @param divs number of points to return
+ * @returns Array of x,y coordinates of <divs> length
+ */
+export const quadraticBezierPoints = (
   x0: number,
   y0: number,
   x1: number,
@@ -33,8 +44,18 @@ export const cubicBezierPoints = (
   y2: number,
   divs: number
 ) => {
-  return interpolate(divs).map(t => ({
-    x: lerp(lerp(x0, x1, t), lerp(x1, x2, t), t),
-    y: lerp(lerp(y0, y1, t), lerp(y1, y2, t), t),
-  }));
+  return interpolate(divs).map(t => quadratic(x0, y0, x1, y1, x2, y2, t));
 };
+
+export const quadratic = (
+  x0: number,
+  y0: number,
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number,
+  t: number
+) => ({
+  x: lerp(lerp(x0, x1, t), lerp(x1, x2, t), t),
+  y: lerp(lerp(y0, y1, t), lerp(y1, y2, t), t),
+});
--- src/wallpapers/bezier.tsx
+++ src/wallpapers/bezier.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cubicBezierPoints, interpolate, lerp } from '../utils';
+import { quadraticBezierPoints } from '../utils';
 import Star from './components/star';
 
 const width = 1920;
@@ -11,29 +11,15 @@ export default function Bezier() {
   return (
     <svg xmlns="http://www.w3.org/svg/2000" viewBox={`0 0 ${width} ${height}`}>
       <rect width={width} height={height} fill="hsl(225,50%,20%)" />
-      {interpolate(20).map(k => (
-        <circle
-          key={k}
-          cx={lerp(lerp(x1, x1, k), lerp(x1, width, k), k)}
-          cy={lerp(lerp(y1, height, k), lerp(height, height, k), k)}
-          r={40}
-          stroke="white"
-          fill="none"
-        />
+      <path d="M50 50 Q50 1030 1870 1030" stroke="white" fill="none" />
+      {quadraticBezierPoints(50, 50, 50, 1030, 1870, 1030, 10).map((p, i) => (
+        <circle key={i} cx={p.x} cy={p.y} r={50} stroke="white" fill="none" />
       ))}
-      {interpolate(20).map(k => (
-        <circle
-          key={k}
-          cx={lerp(lerp(x1, width, k), lerp(width, width, k), k)}
-          cy={lerp(lerp(y1, 0, k), lerp(0, height, k), k)}
-          r={40}
-          stroke="white"
-          fill="none"
-        />
-      ))}
-      {cubicBezierPoints(50, 50, 50, 1030, 1870, 1030, 20).map((p, i) => (
-        <circle key={i} cx={p.x} cy={p.y} r={50} stroke="white" />
-      ))}
+      {quadraticBezierPoints(50, 50, 1870, 50, 1870, 1030, 10)
+        .slice(1, -1)
+        .map((p, i) => (
+          <circle key={i} cx={p.x} cy={p.y} r={50} stroke="white" fill="none" />
+        ))}
     </svg>
   );
 }
