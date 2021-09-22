6033e7abf912ace14ea7ffcdeb009288dce4f88f
David Rose <doppler@gmail.com>
2021-09-22 17:13:15 -0400

    tweek Orbits for production

--- src/wallpapers/Orbits.tsx
+++ src/wallpapers/Orbits.tsx
@@ -1,50 +1,45 @@
 import React from 'react';
 import { pc } from '../utils';
 const PHI = (1 + Math.sqrt(5)) / 2;
-const width = 1920;
-const height = 1080;
+const width = 2400;
+const height = 2400;
 const cx = width / 2;
 const cy = height / 2;
 const earthRadius = (height / 10) * 4;
 const venusRadius = earthRadius * (PHI - 1);
 const degrees = 10;
+
 export default function Orbits() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${width} ${height}`}>
       <defs>
         <filter id="bgFilter">
-          <feTurbulence
-            type="fractalNoise"
-            baseFrequency="0.013"
-            seed="13"
-            result="noise"
-          />
-          <feColorMatrix
-            values="0 0 0.5 0 0
-                    0 0 0 0 0
-                    1 1 1 1 0 
-                    0 0 0 0.15 0"
-          />
+          <feTurbulence type="fractalNoise" baseFrequency={(PHI - 1) ** 2} />
+          <feSpecularLighting
+            specularExponent="20"
+            lightingColor="hsl(240, 50%, 40%)"
+            surfaceScale="1"
+          >
+            <fePointLight x={cx} y={cy} z={cy} />
+          </feSpecularLighting>
         </filter>
         <filter id="blurFilter">
-          <feGaussianBlur stdDeviation="3" in="SourceGraphic" result="blur" />
-          <feComponentTransfer in="blur" result="transfer">
-            <feFuncR type="linear" slope={2} />
-            <feFuncG type="linear" slope={2} />
-            <feFuncB type="linear" slope={2} />
+          <feGaussianBlur stdDeviation="5" in="SourceGraphic" result="blur" />
+          <feColorMatrix type="saturate" values="0.0" result="saturate" />
+          <feComponentTransfer>
+            <feFuncR type="linear" slope="0.5" />
+            <feFuncG type="linear" slope="0.5" />
+            <feFuncB type="linear" slope="2" />
           </feComponentTransfer>
           <feMerge>
-            <feMergeNode in="transfer" />
+            <feMergeNode />
             <feMergeNode in="SourceGraphic" />
           </feMerge>
         </filter>
       </defs>
-      <rect
-        width={width}
-        height={height}
-        filter="url(#bgFilter)"
-        // fill={`hsl(225, 50%, 10%)`}
-      />
+      <rect width={width} height={height} fill="black" />
+
+      <rect width={width} height={height} filter="url(#bgFilter)" />
       <g transform={`rotate(${90}, ${cx}, ${cy})`} filter="url(#blurFilter)">
         {[...Array((360 / degrees) * 8).keys()].map((a, i) => (
           <line
@@ -53,8 +48,21 @@ export default function Orbits() {
             y1={pc(cx, cy, a * degrees, earthRadius).y}
             x2={pc(cx, cy, a * degrees * 1.625, venusRadius).x}
             y2={pc(cx, cy, a * degrees * 1.625, venusRadius).y}
+            stroke={`hsl(${((a * degrees) % 360) - 72}, 100%, 50%)`}
+            strokeWidth={2}
+          />
+        ))}
+        {[...Array(360 / degrees).keys()].map(a => (
+          <path
+            key={a}
+            d={`M${pc(cx, cy, a * degrees - degrees / 2, earthRadius).x},${
+              pc(cx, cy, a * degrees - degrees / 2, earthRadius).y
+            }A${earthRadius},${earthRadius} 0 0 1 ${
+              pc(cx, cy, a * degrees + degrees / 2, earthRadius).x
+            },${pc(cx, cy, a * degrees + degrees / 2, earthRadius).y}`}
+            strokeWidth="5"
             stroke={`hsl(${a * degrees - 72}, 100%, 50%)`}
-            strokeWidth={1}
+            fill="none"
           />
         ))}
       </g>
