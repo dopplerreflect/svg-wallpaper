bb4821f22ec77af6dc956458ed096be81f40c182
David Rose <doppler@gmail.com>
2021-09-28 22:17:39 -0400

    griddy bg

--- src/wallpapers/Paisley.tsx
+++ src/wallpapers/Paisley.tsx
@@ -24,8 +24,13 @@ export default function Paisley() {
       <defs>
         <radialGradient id="radialGradient">
           <stop offset="0%" stopColor={`hsl(${basehue + 180}, 100%, 50%)`} />
-          <stop offset="100%" stopColor={`hsl(${basehue + 270}, 100%, 50%)`} />
+          <stop offset="70%" stopColor={`hsl(${basehue + 270}, 100%, 50%)`} />
+          <stop offset="100%" stopColor={`hsl(${basehue + 270}, 100%, 10%)`} />
         </radialGradient>
+        <linearGradient id="linearGradient" gradientTransform="rotate(-15)">
+          <stop offset="0%" stopColor={`hsl(${basehue + 60}, 100%, 50%)`} />
+          <stop offset="100%" stopColor={`hsl(${basehue + 0}, 100%, 50%)`} />
+        </linearGradient>
         <filter id="blur">
           <feGaussianBlur in="SourceGraphic" stdDeviation="10" />
         </filter>
@@ -41,11 +46,40 @@ export default function Paisley() {
             `M${ps(0, gr[3])}`,
             `A${gr[3]},${gr[3]} 1 1 0 ${ps(-359.9, gr[3])}`,
           ].join()}
-          stroke={`hsl(${basehue + 0}, 100%, 15%)`}
+          stroke={`hsl(${basehue + 30}, 50%, 25%)`}
           strokeWidth={cy / 80}
-          fill={`hsl(${basehue + 30}, 100%, 50%)`}
-          fillOpacity="0.75"
+          // fill={`hsl(${basehue + 30}, 100%, 50%)`}
+          fill="url(#linearGradient)"
+          fillOpacity={0.75}
+          filter="url(#paisleyHexShadow)"
         />
+        <filter id="paisleyHexShadow">
+          <feFlood
+            result="flood"
+            floodColor={`hsl(${basehue + 30}, 100%, 0%)`}
+            floodOpacity={1}
+          />
+          <feComposite
+            in="flood"
+            in2="SourceGraphic"
+            operator="in"
+            result="mask"
+          />
+          <feMorphology
+            in="mask"
+            operator="dilate"
+            radius={cy / 80}
+            result="dilated"
+          />
+          <feGaussianBlur in="dilated" stdDeviation={cy / 40} result="blur" />
+          <feMerge>
+            <feMergeNode in="blur" />
+            <feMergeNode in="SourceGraphic" />
+          </feMerge>
+        </filter>
+        <filter id="stripeBlur">
+          <feGaussianBlur stdDeviation={cy / 600} />
+        </filter>
       </defs>
       <rect
         width={width}
@@ -65,30 +99,54 @@ export default function Paisley() {
           filter="url(#blur)"
         />
       ))}
-      {angles.map(
-        a =>
-          a % 120 === 0 && (
-            <use
-              key={a}
-              xlinkHref="#paisley"
-              transform={`rotate(${a}, ${cx}, ${cy}), translate(${
-                cy * PHIm1 ** 2
-              })`}
+      <g id="stripes" filter="url(#stripeBlur)">
+        {[...Array(cy / 3).keys()].map(k => (
+          <g key={k}>
+            <line
+              x1={0}
+              y1={k * 6}
+              x2={width}
+              y2={k * 6}
+              stroke={`hsl(${basehue + 0 + k * 2}, 100%, 20%)`}
+              strokeWidth={cy / 540}
             />
-          )
-      )}
-      {angles.map(
-        a =>
-          a % 120 === 60 && (
-            <use
-              key={a}
-              xlinkHref="#paisley"
-              transform={`rotate(${a}, ${cx}, ${cy}), translate(${
-                cy * PHIm1 ** 2
-              })`}
+            <line
+              x1={k * 6}
+              y1={0}
+              x2={k * 6}
+              y2={height}
+              stroke={`hsl(${basehue + 180 + k * 2}, 100%, 20%)`}
+              strokeWidth={cy / 540}
             />
-          )
-      )}
+          </g>
+        ))}
+      </g>
+      <g id="paisleyHex">
+        {angles.map(
+          a =>
+            a % 120 === 0 && (
+              <use
+                key={a}
+                xlinkHref="#paisley"
+                transform={`rotate(${a}, ${cx}, ${cy}), translate(${
+                  cy * PHIm1 ** 2
+                })`}
+              />
+            )
+        )}
+        {angles.map(
+          a =>
+            a % 120 === 60 && (
+              <use
+                key={a}
+                xlinkHref="#paisley"
+                transform={`rotate(${a}, ${cx}, ${cy}), translate(${
+                  cy * PHIm1 ** 2
+                })`}
+              />
+            )
+        )}
+      </g>
     </svg>
   );
 }
