1422409d376a9b43b6146e06ee8514099ffed83e
David Rose <doppler@gmail.com>
2021-12-10 10:38:15 -0600

    use colorMatrix filters

--- src/wallpapers/SpiralStar.tsx
+++ src/wallpapers/SpiralStar.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Star from './components/star';
 import useSaveSVG from '@dopplerreflect/use-save-svg';
-import { outerRadius } from './components/Blueprint';
 const PHI = (1 + Math.sqrt(5)) / 2;
 const width = 1080;
 const height = 2375;
 const cx = width / 2;
 const cy = height / 2;
 const radii = Array.from(Array(12)).map((_, i) => (width / (PHI * 2)) * (PHI - 1) ** i);
-// const angles = Array.from(Array(10)).map((_, i) => i * (360 / 10));
 const pc = (angle: number, radius: number) => {
   return {
     x: cx + radius * Math.cos(angle * (Math.PI / 180)),
@@ -29,39 +27,61 @@ export default function SpiralStar() {
       ref={svgRef}
       id='Manufactory-Main'
       xmlns='http://www.w3.org/2000/svg'
-      // viewBox={`0 0 ${width} ${height}`}
-      viewBox='0 768 1024 1024'
+      viewBox={`0 0 ${width} ${height}`}
+      // viewBox='0 768 1024 1024'
     >
       <defs>
-        <linearGradient id='gradient' gradientTransform='rotate(90)'>
-          <stop offset='0%' stopColor='hsl(280, 100%, 50%)' />
-          <stop offset='100%' stopColor='hsl(220, 100%, 50%)' />
-        </linearGradient>
-        <clipPath id='circleClip'>
-          <circle cx={cx} cy={cy} r={width / (PHI * 2)} />
+        <clipPath id='starClip'>
+          <Star cx={cx} cy={cy} r={radii[0]} />
         </clipPath>
+        <mask id='outsideStarMask'>
+          <circle cx={cx} cy={cy} r={radii[0]} fill='white' />
+          <Star cx={cx} cy={cy} r={radii[0]} fill='black' />
+        </mask>
         <mask id='circleMask'>
           <rect width={width} height={height} fill='white' />
           <circle cx={cx} cy={cy} r={width / (PHI * 2)} fill='black' />
         </mask>
+
+        <filter id='colorFilter1'>
+          <feColorMatrix type='hueRotate' values='30' />
+        </filter>
+        <filter id='colorFilter2'>
+          <feColorMatrix type='hueRotate' values='-90' />
+        </filter>
+        <g id='starfield'>
+          <StarField startHue={360} endHue={180} outerBounds={cy} />
+        </g>
       </defs>
-      <g id='sunflower-bg' mask='url(#circleMask)' fillOpacity={0.5}>
-        <StarField startHue={360} endHue={180} outerBounds={cy} />
-      </g>
-      <g
-        id='circles'
-        transform={`rotate(-90, ${cx}, ${cy})`}
-        stroke={`hsl(30, 100%, 50%)`}
-        strokeWidth={height / 1080}
-        fill='none'
-      >
-        {radii.map((r, i) => (
+
+      <rect x={0} y={0} width={width} height={height} fill='white' />
+
+      <g id='circles' stroke={`hsl(30, 100%, 50%)`} strokeWidth={height / 1080} fill='none'>
+        {radii.map(r => (
           <circle key={r} cx={cx} cy={cy} r={r} />
         ))}
       </g>
-      <Star cx={cx} cy={cy} r={radii[0]} fill='url(#gradient)' fillOpacity={0.75} />
-      <g id='sunflower' clipPath='url(#circleClip)' fillOpacity={0.5}>
-        <StarField startHue={45} endHue={0} outerBounds={radii[0]} />
+
+      <g id='sunflower-bg' mask='url(#circleMask)' fillOpacity={0.5}>
+        <use xlinkHref='#starfield' />
+      </g>
+
+      <Star
+        cx={cx}
+        cy={cy}
+        r={radii[0]}
+        fillOpacity={0.5}
+        fill='white'
+        stroke='hsl(30, 100%, 50%)'
+        strokeWidth={height / 1080}
+      />
+
+      <g id='sunflowerInCircle' mask='url(#outsideStarMask)' fillOpacity={0.75}>
+        <use xlinkHref='#starfield' filter='url(#colorFilter1)' />
+      </g>
+
+      <g id='sunflowerInStar' clipPath='url(#starClip)' fillOpacity={0.5}>
+        <use xlinkHref='#starfield' filter='url(#colorFilter2)' />
       </g>
     </svg>
   );
