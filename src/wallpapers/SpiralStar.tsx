c6f339a6dc783a91478fa8ebeb40494330cf37a2
David Rose <doppler@gmail.com>
2022-01-31 10:09:55 -0800

    wallpaper tweeks

--- src/wallpapers/SpiralStar.tsx
+++ src/wallpapers/SpiralStar.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Star from './components/star';
 import useSaveSVG from '@dopplerreflect/use-save-svg';
 const PHI = (1 + Math.sqrt(5)) / 2;
-const width = 1080;
-const height = 2375;
+const width = 1920;
+const height = 1080;
 const cx = width / 2;
 const cy = height / 2;
-const radii = Array.from(Array(12)).map((_, i) => (width / (PHI * 2)) * (PHI - 1) ** i);
+// const radii = Array.from(Array(12)).map((_, i) => (width / (PHI * 2)) * (PHI - 1) ** i);
+const radii = Array.from(Array(12)).map((_, i) => cy * (PHI - 1) ** i);
 const pc = (angle: number, radius: number) => {
   return {
     x: cx + radius * Math.cos(angle * (Math.PI / 180)),
@@ -40,7 +41,7 @@ export default function SpiralStar() {
         </mask>
         <mask id='circleMask'>
           <rect width={width} height={height} fill='white' />
-          <circle cx={cx} cy={cy} r={width / (PHI * 2)} fill='black' />
+          <circle cx={cx} cy={cy} r={cy} fill='black' />
         </mask>
 
         <filter id='circleHueShift'>
@@ -50,31 +51,31 @@ export default function SpiralStar() {
           <feColorMatrix type='hueRotate' values='30' />
         </filter>
         <g id='starfield'>
-          <StarField startHue={60} endHue={320} outerBounds={cy} />
+          <StarField startHue={0} endHue={360} outerBounds={cx} />
         </g>
       </defs>
 
-      <rect x={0} y={0} width={width} height={height} fill='white' />
+      <rect x={0} y={0} width={width} height={height} fill='black' />
 
-      {/* <g id='circles' stroke={`hsl(30, 100%, 50%)`} strokeWidth={height / 1080} fill='none'>
+      <g id='circles' stroke={`hsl(30, 100%, 50%)`} strokeWidth={height / 1080} fill='none'>
         {radii.map(r => (
           <circle key={r} cx={cx} cy={cy} r={r} />
         ))}
-      </g> */}
+      </g>
 
-      <g id='sunflower-bg' mask='url(#circleMask)' fillOpacity={0.5}>
+      <g id='sunflower-bg' mask='url(#circleMas)' fillOpacity={0.5}>
         <use xlinkHref='#starfield' />
       </g>
 
-      {/* <Star
+      <Star
         cx={cx}
         cy={cy}
         r={radii[0]}
-        fillOpacity={0.5}
-        fill='white'
+        fillOpacity={0.1}
+        // fill='white'
         stroke='hsl(30, 100%, 50%)'
         strokeWidth={height / 1080}
-      /> */}
+      />
 
       <g id='sunflowerInCircle' mask='url(#outsideStarMask)' fillOpacity={0.75}>
         <use xlinkHref='#starfield' filter='url(#circleHueShift)' />
@@ -96,10 +97,10 @@ const StarField = ({ outerBounds, startHue, endHue }: StarFieldProps) => {
   let i = 0;
   let r = 0;
   let paramsArray = [];
-  while (r < outerBounds + outerBounds * 0.1) {
+  while (r < outerBounds + outerBounds * 0.2) {
     i++;
     let angle = (i * (360 - 360 * (PHI - 1))) % 360;
-    r = ((cy - cy * 0.9) / 300) * i;
+    r = ((cy - cy * 0.9) / 144) * i;
     const c = p(angle, r);
     paramsArray.push({ i, c, r, angle });
   }
@@ -112,12 +113,12 @@ const StarField = ({ outerBounds, startHue, endHue }: StarFieldProps) => {
       r={p.r / (13 + 0.015 * i)}
       rotate={p.angle}
       stroke={`hsl(${
-        startHue - Math.abs((startHue - endHue) / paramsArray.length) * i
-      }, 100%, 25%)`}
+        startHue - Math.abs((startHue + endHue) / paramsArray.length) * i
+      }, 100%, 20%)`}
       // stroke='black'
       strokeWidth={0.75 + p.r / 540}
       fill={`hsl(${
-        startHue - Math.abs((startHue - endHue) / paramsArray.length) * i
+        startHue + Math.abs((startHue - endHue) / paramsArray.length) * i
       }, 100%, 50%)`}
     />
   ));
